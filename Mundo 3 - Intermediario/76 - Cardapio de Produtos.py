24d97a0a524196f81fb8df47895d607b0af3e94c
Lucas Matheus Costa <teclucas.costa@hotmail.com>
2024-04-12 12:20:04 -0300

    <refactor> - Incusão do exercicio 76
    Versão: 1.1.2:04-12042024
    Descrição: Inclusão do exercicio 76

--- Mundo 3 - Intermediario/76 - Cardapio de Produtos.py
+++ Mundo 3 - Intermediario/76 - Cardapio de Produtos.py
@@ -7,6 +7,12 @@
 stringDeColecao = ""
 indexadorDeControle = True
 
+
+print("""
+      DICA: Para fechar o programa, digite \"666\" no Nome do produto
+      ===========================================================
+      """)
+
 while indexadorDeControle==True:
 
     # Declaração de variáveis
@@ -15,23 +21,24 @@ while indexadorDeControle==True:
     
     resposta = input("Qual o nome do produto : ")    
 
+    # Quebrar o programa
+    if resposta=="666":
+        indexadorDeControle = False
+        break
+
     # Concatenando
     stringDeColecao += resposta+","
      
     resposta = input("Qual o preço : R$ ")
     stringDeColecao += resposta
 
-    # Gatilho da repetição
-    gatilhoDeControle = input("Você quer continuar montando o cardapio (S/N) : ")
-    if (gatilhoDeControle == ""):
-        gatilhoDeControle = "n"
-    gatilhoDeControle = gatilhoDeControle[0].lower()
-
-    if gatilhoDeControle != "s":
-        indexadorDeControle = False
-    else:
-        stringDeColecao += ","
     print("="*40)
 
+# 2º Passo -  Convertendo em Tupla
+cardapio = stringDeColecao.split(",") # Array<List>
+cardapio = tuple(cardapio)
 
-cardapio = () # Array<Tupla>
+# 3º Passo - Imprimir o cardapio
+print("==========================")
+for indice in range(0,len(cardapio)-1,2):
+    print("Item : {} | Preço: R$ {}".format(cardapio[indice],cardapio[indice+1]))
